e272e4e7b0c9184e474cedf2604bbbadf79ec6cf
Parlee Khadka <khadkaparlee@gmail.com>
2025-02-24 04:42:08 -0800

    first secondthidadaadd

--- chesshack/settings.py
+++ chesshack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qjt7#7zb-60blq5480z)*o!0&*9sa@-_!w2+&spjizx4jt(c$=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chess-message.vercel.app','chess.com']
 
 
 # Application definition
